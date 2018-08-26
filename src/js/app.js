7b17ab788d9453794c330b378e07f4472c425f58
kari14 <kari.mimisson@gmail.com>
2018-08-26 20:16:40 +0200

    small changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,65 +2,104 @@ var Draumurinn = angular.module('Draumurinn', []);
 
 Draumurinn.controller('FantasyController', ['$scope', function($scope) {
     $scope.cash = 10000000;
-    $scope.messages = 'Draumurinn';
-    $scope.addPlayer = function(player) {
-        //var currentPlayer = $scope.players.indexOf(player);
-        if ($scope.myTeam.length === 3) {
-            //ojjjj barasta!!!
-            alert("Team is full");
-            return
+    $scope.points = 0;
+    //Index of the player that is going out.
+    $scope.playerOut = '';
+
+    $scope.confirmTeam = function() {
+        var set = new Set();
+        for (var i in $scope.myTeam) {
+            if (set.has(i.team)) {
+                alert('Too many players from the same team');
+                return false;
+            }
         }
-            $scope.myTeam.push(player);
+
+        if ($scope.cash < 0) {
+            alert('You cant afford this team');
+            return false;
+        }
+
+        if ($scope.myTeam.length != 4) {
+            alert('Need to fill your team');
+            return false;
+        }
+
+        return true;
+    }
+
+    $scope.setPlayerOut = function(player) {
+        $scope.playerOut = player;
+    }
+
+    $scope.transfer = function(player) {
+        if ($scope.playerOut !== '') {
+            $scope.myTeam[$scope.playerOut] = player;
             $scope.cash -= player.price;
+        }
     }
-
-    $scope.removePlayer = function(player) {
-        var current = $scope.myTeam.indexOf(player);
-        $scope.myTeam.splice(current, 1);
-    }
-
 
     $scope.players = [
     {
         name: 'Kári',
         price: 5000000,
         team: 'KR',
-        pic: 'src/img/avatar.jpg'
+        pic: 'src/img/avatar.jpg',
+        points: 0
     },
     {
         name: 'Tommi',
         price: 1000000,
         team: 'BH',
-        pic: 'src/img/avatar.jpg'
+        pic: 'src/img/avatar.jpg',
+        points: 0
     },
     {
         name: 'Jói',
         price: 3000000,
         team: 'BH',
-        pic: 'src/img/avatar.jpg'
+        pic: 'src/img/avatar.jpg',
+        points: 0
     },
     {
         name: 'Pétur',
         price: 1000000,
         team: 'BH',
-        pic: 'src/img/avatar.jpg'
+        pic: 'src/img/avatar.jpg',
+        points: 0
     },
     {
         name: 'Bjarni Bjarna',
         price: 1000000,
         team: 'HK',
-        pic: 'src/img/avatar.jpg'
+        pic: 'src/img/avatar.jpg',
+        points: 0
     },
     {
         name: 'Maggi K',
         price: 5000000,
         team: 'Víkingur',
-        pic: 'src/img/avatar.jpg'
+        pic: 'src/img/avatar.jpg',
+        points: 0
+    },
+    {
+        name: 'Gestur',
+        price: 500000,
+        team: 'KR-B',
+        pic: 'src/img/avatar.jpg',
+        points: 0
+    }
+    
+    ];
 
-    }];
+    var emptyPlayer = {
+        name: '',
+        price: 0,
+        team: '',
+        pic: 'src/img/avatar.jpg',
+        points: 0
+    }
 
-    $scope.myTeam = [];
-
-    $scope.reserve = "";
+    $scope.myTeam = [emptyPlayer, emptyPlayer, emptyPlayer, emptyPlayer];
 
 }]);
