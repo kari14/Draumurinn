b15ed4be2963e119a2090879baa49625bc310762
kari14 <kari.mimisson@gmail.com>
2018-08-26 15:00:53 +0200

    proof of concept

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,25 @@
 var Draumurinn = angular.module('Draumurinn', []);
 
 Draumurinn.controller('FantasyController', ['$scope', function($scope) {
-    $scope.message = 'Draumurinn';
+    $scope.cash = 10000000;
+    $scope.messages = 'Draumurinn';
+    $scope.addPlayer = function(player) {
+        //var currentPlayer = $scope.players.indexOf(player);
+        if ($scope.myTeam.length === 3) {
+            //ojjjj barasta!!!
+            alert("Team is full");
+            return
+        }
+            $scope.myTeam.push(player);
+            $scope.cash -= player.price;
+    }
+
+    $scope.removePlayer = function(player) {
+        var current = $scope.myTeam.indexOf(player);
+        $scope.myTeam.splice(current, 1);
+    }
+
+
     $scope.players = [
     {
         name: 'Kári',
@@ -38,7 +56,11 @@ Draumurinn.controller('FantasyController', ['$scope', function($scope) {
         price: 5000000,
         team: 'Víkingur',
         pic: 'src/img/avatar.jpg'
-    }
-]
+
+    }];
+
+    $scope.myTeam = [];
+
+    $scope.reserve = "";
 
 }]);
